f26a938144c0fd0cc46f3314d42e0ec9b6404b70
neojp <neojp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-20 21:32:08 +0000

    Version & Tag 1.1
    * Rename gf.placeholders.js.php to gf.placeholders.js
    * Append a jquery.placeholder.js url as a js variable on the <head> tag instead of using php on gf.placeholders.js.php - improves performance by using static files
    * Always request a cached version of jquery.placeholder-1.0.1.js
    * Remove Modernizr support, use our simpler conditional - js performance improvement
    * Improved input and textarea filtering, added support for new HTML5 input types, eg. email, url, tel
    * Improved Javascript performance
    
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-placeholders/trunk@534295 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravityforms-placeholders.php
+++ gravityforms-placeholders.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms - Placeholders add-on
 Plugin URI: http://github.com/neojp/gravity-forms-placeholders/
 Description: Adds HTML5 placeholder support to Gravity Forms' fields with a javascript fallback. Javascript & jQuery are required.
-Version: 1.0
+Version: 1.1
 Author: Joan Piedra
 Author URI: http://joanpiedra.com
 
@@ -12,7 +12,12 @@ Just add a "gplaceholder" CSS classname to the required fields.
 
 */
 
-if (is_admin() || (isset($GLOBALS['pagenow']) && $GLOBALS['pagenow'] == 'wp-login.php'))
+if ( isset( $GLOBALS['pagenow'] ) && $GLOBALS['pagenow'] == 'wp-login.php' )
 	return;
 
-wp_enqueue_script("_gf_placeholders", plugins_url(null, __FILE__) . "/gf.placeholders.js.php", array("jquery"), "1.0");
+add_action('wp_print_scripts', 'gf_placeholder_enqueue_scripts');
+
+function gf_placeholder_enqueue_scripts() {
+	echo "<script>var jquery_placeholder_url = '" . plugins_url('/jquery.placeholder-1.0.1.js', __FILE__) . "';</script>";
+	wp_enqueue_script('_gf_placeholders', plugins_url('/gf.placeholders.js', __FILE__), array('jquery'), '1.0' );
+}
